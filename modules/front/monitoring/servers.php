006f0b9ca3e0aaa85d2c9c22770d23ae0b39d424
Firdavs Khaydarov <recouse@mail.ru>
2017-02-13 09:58:05 +0500

    Added Server active record

--- modules/front/monitoring/servers.php
+++ modules/front/monitoring/servers.php
@@ -33,34 +33,31 @@ class _servers extends \IPS\Dispatcher\Controller
 	 */
 	protected function manage()
 	{
-		if ( \IPS\Member::loggedIn()->inGroup( explode( ",", \IPS\Settings::i()->monitoring_groups ) ) )
+		/* Update session location */
+		\IPS\Session::i()->setLocation( \IPS\Http\Url::internal( 'app=monitoring', 'front', 'monitoring' ), array(), 'loc_monitoring_viewing_monitoring' );
+
+		/* Check permissions */
+		if ( !\IPS\Member::loggedIn()->inGroup( explode( ",", \IPS\Settings::i()->monitoring_groups ) ) )
 		{
-			\IPS\Output::i()->title = \IPS\Member::loggedIn()->language()->addToStack( '__app_monitoring' );
-
-			/* Update session location */
-			\IPS\Session::i()->setLocation( \IPS\Http\Url::internal( 'app=monitoring', 'front', 'monitoring' ), array(), 'loc_monitoring_viewing_monitoring' );
-
-
-			//if( empty( \IPS\Data\Store::i()->server_info ) )
-				//\IPS\Task::constructFromData(\IPS\Db::i()->select('*', 'core_tasks', array('app=? AND `key`=?', 'monitoring', 'monitoringUpdate'))->first())->run();
-
-			$serverInfo = \IPS\Data\Store::i()->server_info['data'];
-			$serverInfoOther = \IPS\Data\Store::i()->server_info['other'];
-			$lastUpdate = \IPS\DateTime::ts( \IPS\Data\Store::i()->server_info['fetched'] )->html();
-			$allPlayers = array(
-				'AllPlayers' => $serverInfo['AllPlayers'],
-				'MaxAllPlayers' => $serverInfo['MaxAllPlayers'],
-				'AllFill' => $serverInfo['AllFill'],
-			);
-			unset( $serverInfo['AllPlayers'], $serverInfo['MaxAllPlayers'], $serverInfo['AllFill'] );
-
-			/* Display */
-			\IPS\Output::i()->output = \IPS\Theme::i()->getTemplate( 'server', 'monitoring', 'front' )->serverInfo( $serverInfo, $serverInfoOther, $lastUpdate, $allPlayers ) . \IPS\Application::load( 'monitoring' )->copyright();
+			\IPS\Output::i()->error( 'no_module_permission', '1SM/0', 403, '' );
 		}
-		else
-		{
-			\IPS\Output::i()->error( 'no_module_permission', '2G188/9', 403, '' );
-		}
+
+		//if( empty( \IPS\Data\Store::i()->server_info ) )
+			//\IPS\Task::constructFromData(\IPS\Db::i()->select('*', 'core_tasks', array('app=? AND `key`=?', 'monitoring', 'monitoringUpdate'))->first())->run();
+
+		$serverInfo = \IPS\Data\Store::i()->server_info['data'];
+		$serverInfoOther = \IPS\Data\Store::i()->server_info['other'];
+		$lastUpdate = \IPS\DateTime::ts( \IPS\Data\Store::i()->server_info['fetched'] )->html();
+		$allPlayers = array(
+			'AllPlayers' => $serverInfo['AllPlayers'],
+			'MaxAllPlayers' => $serverInfo['MaxAllPlayers'],
+			'AllFill' => $serverInfo['AllFill'],
+		);
+		unset( $serverInfo['AllPlayers'], $serverInfo['MaxAllPlayers'], $serverInfo['AllFill'] );
+
+		/* Display */
+		\IPS\Output::i()->title 	= \IPS\Member::loggedIn()->language()->addToStack( '__app_monitoring' );
+		\IPS\Output::i()->output 	= \IPS\Theme::i()->getTemplate( 'server', 'monitoring', 'front' )->serverInfo( $serverInfo, $serverInfoOther, $lastUpdate, $allPlayers ) . \IPS\Application::load( 'monitoring' )->copyright();
 	}
 	
 	// Create new methods with the same name as the 'do' parameter which should execute it
